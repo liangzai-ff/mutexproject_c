f51a92a162178e82130e791a663ab4f44ed6df9b
村里修摩托最靓的仔 <40228542+paVVN@users.noreply.github.com>
2023-06-12 10:37:46 +0800

    Create mutex.cpp

---
+++ mutex.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+
+//checking mutex
+BOOL IsAlreadyRun()
+{
+    HANDLE hMutex = NULL;
+    hMutex = ::CreateMutex(NULL, FALSE, L"check_v");
+    if (hMutex)
+    {
+        if (ERROR_ALREADY_EXISTS == ::GetLastError())
+        {
+            MessageBoxW(NULL, L"傻屌，程序已经开始运行啦！", L"Message", MB_OK | MB_ICONINFORMATION);
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+int main()
+{
+    if (IsAlreadyRun())
+    {
+        return 0;
+    }
+    system("pause");
+    return 0;
+    
+}
+
